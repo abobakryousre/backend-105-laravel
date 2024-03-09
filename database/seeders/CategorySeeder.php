f931a93f8fcd4202655785462d6a49af95ee332a
Abobakr Yousry <abobakr.yousry@vodafone.com>
2024-03-09 13:56:49 +0200

    create category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,18 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
-use Illuminate\Support\Facades\DB;
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ProductSeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Product::factory()->count(10)->create();
+        Category::factory()->count(2)->create();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Database\Seeders\ProductSeeder;
+use Database\Seeders\CategorySeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,6 +22,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             ProductSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
