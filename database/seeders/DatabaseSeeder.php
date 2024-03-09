c5739eb17ef9db0a72fc7a5adf8f5f067874eefc
Abobakr Yousry <abobakr.yousry@vodafone.com>
2024-03-09 12:03:01 +0200

    add product seeder to DB seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\ProductSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,5 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            ProductSeeder::class,
+        ]);
     }
 }
